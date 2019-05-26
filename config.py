73a3b3b085a09e8d7ea20cd2892e75f6fbd85e57
Yujin <yujinjcho@gmail.com>
2019-05-25 19:48:35 -0700

    remove tokens from config

--- config.py
+++ config.py
@@ -16,7 +16,5 @@ GOOGLE_CLIENT_SECRET_CONFIG = env['GOOGLE_CLIENT_SECRET_CONFIG']
 GOOGLE_AUTH = {
     'client_id': env['GOOGLE_CLIENT_ID'],
     'client_secret': env['GOOGLE_CLIENT_SECRET'],
-    'access_token': env['GOOGLE_ACCESS_TOKEN'],
-    'refresh_token': env['GOOGLE_REFRESH_TOKEN'],
     'auth_redirect': env['GOOGLE_AUTH_REDIRECT']
 }
